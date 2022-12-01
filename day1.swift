25ba16f75950a8ea97df36103c3b1aaf257f7a0e
Gianpiero Spinelli <gianpispi@me.com>
2022-12-01 10:10:16 +0100

    Day 1

---
+++ day1.swift
@@ -0,0 +1,22 @@
+import Foundation
+
+let input = """
+<#Your input#>
+"""
+
+let elvesArray = input.components(separatedBy: "\n\n")
+let sums = elvesArray
+  .map {
+    $0.components(separatedBy: "\n")
+      .map{ Int($0)! }
+      .reduce(0, +)
+  }
+
+print("Part 1: ", sums.max()!)
+
+// part 2
+let partTwo = sums
+  .sorted(by: >)
+  .prefix(3)
+  .reduce(0, +)
+print("Part 2:", partTwo)
